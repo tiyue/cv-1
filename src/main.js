7ac44385dcfa85a750e9888e2fed5e42a8cb15f8
kkbox <1614689712@qq.com>
2019-12-09 21:08:59 +0800

    初次提交

---
+++ src/main.js
@@ -0,0 +1,70 @@
+let html = document.querySelector("#html");
+let style = document.querySelector("#style");
+let string = `/*你好，我是一名前端新人
+*演示一下前端功底
+*准备div*/
+
+#div1{
+    border: 1px solid red;
+    width: 200px;
+    height: 200px;
+}
+/*接下来 把div变成一个太极
+*首先把div变成圆
+*/
+#div1{
+    border-radius: 50%; 
+    box-shadow: 0 0 3px rgba(0,0,0,0.5);
+    border:none;
+}
+/*把圆变成黑白相间
+*/
+#div1{
+    background: linear-gradient(90deg, rgba(255,255,255,1) 0%, rgba(255,255,255,1) 50%, rgba(0,0,0,1) 50%, rgba(0,0,0,1) 100%);
+}
+/*加上太极的对称圆
+*/
+#div1::before{
+    width:100px;
+    height:100px;
+    top: 0;
+    left:50%;
+    transform: translateX(-50%);
+    background: radial-gradient(circle, rgba(255,255,255,1) 0%, rgba(255,255,255,1) 25%, rgba(0,0,0,1) 25%, rgba(0,0,0,1) 100%);
+    border-radius:50%;
+}
+#div1::after{
+    width:100px;
+    height:100px;
+    bottom: 0;
+    left:50%;
+    transform: translateX(-50%);
+    background: radial-gradient(circle, rgba(0,0,0,1) 0%, rgba(0,0,0,1) 25%, rgba(255,255,255,1) 25%, rgba(255,255,255,1) 100%);
+    border-radius:50%;
+}
+`
+let string2 = "";
+console.log(string.length);
+let n = 0;
+
+
+let step = () => {
+    setTimeout(() => {
+        if (string[n] === "\n") {
+            string2 += "<br>";
+        } else if (string[n] === " ") {
+            string2 += "&nbsp;";
+        } else {
+            string2 += string[n]
+        }
+        html.innerHTML = string2;
+        style.innerHTML = string.substring(0, n + 1);
+        window.scrollTo(0, 99999);
+        html.scroll(0, 9999);
+        if (n < string.length - 1) {
+            n += 1;
+            step();
+        }
+    }, 0);
+};
+step();
